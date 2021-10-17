8ee80787436dc5b6f8bb21d2d86f90bd3981f7ad
matveisch <matvesch@protonmail.com>
2021-10-17 20:28:20 +0400

    v.1.0.2

--- src/skeleton.js
+++ src/skeleton.js
@@ -2,23 +2,41 @@ let domManip = (function () {
    'use strict';
     let mainDiv = document.getElementById('content');
 
+    // create div for left menu and todos list
     let outerDiv = document.createElement('div');
     outerDiv.classList.add('outer-div-style');
 
+    // creating left menu
     let insideDivLeft = document.createElement('div');
     insideDivLeft.classList.add('inside-div-left');
-    insideDivLeft.textContent = 'Left Div'
 
+    let todayDiv = document.createElement('div');
+    todayDiv.textContent = 'today';
+    todayDiv.classList.add('left-menu-divs');
+    insideDivLeft.appendChild(todayDiv);
+
+    let allTodos = document.createElement('div');
+    allTodos.textContent = 'all todos';
+    allTodos.classList.add('left-menu-divs');
+    insideDivLeft.appendChild(allTodos);
+
+    // creating list with todos
     let insideDivRight = document.createElement('div');
     insideDivRight.classList.add('inside-div-right');
-    insideDivRight.textContent = 'Right Div'
 
+    let createButton = document.createElement('div');
+    createButton.innerHTML = 'create new task';
+    createButton.classList.add('todos');
+    insideDivRight.appendChild(createButton);
+
+    // append everything
     mainDiv.appendChild(outerDiv);
     outerDiv.appendChild(insideDivLeft);
     outerDiv.appendChild(insideDivRight);
 
     return {
-     
+         insideDivRight: insideDivRight,
+         insideDivLeft: insideDivLeft
     }
 })()
 
---
+++ src/todos.js
@@ -0,0 +1,12 @@
+let objectCreate = (function() {
+    const todosFactory = (name, status, date, note) => {
+        name: name
+        status: status
+        date: date
+        note: note
+    }
+
+    return {
+
+    }
+})()
